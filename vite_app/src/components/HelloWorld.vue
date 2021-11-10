f5e06dba1df4795a5b6b514635c4c2a45f88d29a
kasapeeee <sea.of.clouds55@gmail.com>
2021-11-10 21:34:20 +0900

    add-component-with-v-model-and-v-on

--- vite_app/src/components/HelloWorld.vue
+++ vite_app/src/components/HelloWorld.vue
@@ -2,6 +2,11 @@
   <div class="alert alert-info">
     <h2>{{ title }}</h2>
     <p>{{ message }}</p>
+    <hr>
+    <div>
+      <input type="text" class="form-control" v-model="input">
+      <button class="btn btn-info mt-2" v-on:click="doAction">Click</button>
+    </div>
   </div>
 </template>
 
@@ -14,7 +19,13 @@ export default {
   },
   data() {
     return {
-      count: 0
+      message: 'お名前は？',
+      input: 'no-name'
+    }
+  },
+  methods: {
+    doAction() {
+      this.message = 'こんにちは、' + this.input + 'さん！'
     }
   }
 }
