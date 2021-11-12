edf304a3ead6c3cad4cc3aeb97b7f5b7b078a344
kasapeeee <sea.of.clouds55@gmail.com>
2021-11-12 23:29:29 +0900

    add-ref-for-html

--- vite_app/src/components/HelloWorld.vue
+++ vite_app/src/components/HelloWorld.vue
@@ -1,31 +1,27 @@
 <template>
   <div class="alert alert-info">
     <h2>{{ title }}</h2>
-    <p>{{ message }}</p>
-    <hr>
-    <div>
-      <input type="text" class="form-control" v-model="input">
-      <button class="btn btn-info mt-2" v-on:click="doAction">Click</button>
-    </div>
+    <p ref="msg">{{ message }}</p>
+    <button class="btn btn-primary" v-on:click="doAction">Click</button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    title: String,
-  },
   data() {
     return {
-      message: 'お名前は？',
-      input: 'no-name'
+      title: 'Hello World',
+      message: 'This is sample message'
     }
+  },
+  mounted() {
+    this.counter = 0
   },
   methods: {
     doAction() {
-      this.message = 'こんにちは、' + this.input + 'さん！'
-      this.$emit('result-event', this.input)
+      this.counter++
+      this.$refs.msg.innerHTML += '<h6>counted: ' + this.counter + '</h6>'
     }
   }
 }
