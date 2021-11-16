dc95ec7f3d8e2f5a9d517e8b33cb28e6da72471a
kasapeeee <sea.of.clouds55@gmail.com>
2021-11-16 23:37:51 +0900

    add-app-vue

--- calcApp/src/App.vue
+++ calcApp/src/App.vue
@@ -1,15 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <div id="app">
+    <Calc v-bind:title="message" v-on:result-event="appAction">
+      <div class="mt-3 text-left">
+        <table class="table" v-html="log"></table>
+      </div>
+      <div>
+        <button class="btn btn-primary" v-on:click="doClear">Clear Log</button>
+      </div>
+    </Calc>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Calc from './components/Calc.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Calc
+  },
+  data() {
+    return {
+      message: 'CALC',
+      result: [],
+    }
+  },
+  computed: {
+    log() {
+      let table = '<tr><th>Expression</th><th>Value</th></tr>'
+      if(this.result.length > 0) {
+        for ( var i in this.result) {
+          table += '<tr><td>' + this.result[i][0] + '</td><th>'
+            + this.result[i][1] + '</th></tr>'
+        }
+      }
+      return table
+    }
   }
+
+
 }
 </script>
