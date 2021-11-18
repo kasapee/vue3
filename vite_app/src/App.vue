65a02128bc86e8ce3fc9cfca4a8f233640ef9795
kasapeeee <sea.of.clouds55@gmail.com>
2021-11-18 23:25:47 +0900

    add-jsx-props

--- vite_app/src/App.vue
+++ vite_app/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloJSX />
+    <HelloJSX title="OK, Vue3" message="※属性で設定したメッセージ。"/>
   </div>
 </template>
 
--- vite_app/src/components/helloJSX.jsx
+++ vite_app/src/components/helloJSX.jsx
@@ -2,15 +2,19 @@ export default {
     name: 'HelloJSX',
     data() {
         return {
-            title: 'Hello JSX',
-            message: 'This is sample message',
+            cls_title: 'text-danger h1',
+            cls_message: 'text-primary h5',
         }
+    },
+    props: {
+        title: String,
+        message: String
     },
     render(h) {
         return (
             <div class="aleart alert-primary">
-                <h2>{ this.title }</h2>
-                <p>{ this.message }</p>
+                <h2 class={this.cls_title}>{ this.title }</h2>
+                <p class={this.cls_message}>{ this.message }</p>
             </div>
         )
     }
