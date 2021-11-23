8d7bbeb59967b4711e1de6d52bc893835d646e81
kasapeeee <sea.of.clouds55@gmail.com>
2021-11-23 09:34:26 +0900

    add-compostion-api-jsx

--- vite_app/src/App.vue
+++ vite_app/src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld title="Composition API" msg="This is Composition API sample" />
+    <HelloJSX title="Composition API JSX" msg="This is Composition API JSX"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import HelloJSX from './components/HelloJSX.jsx'
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
+    HelloJSX
   },
 }
 </script>
