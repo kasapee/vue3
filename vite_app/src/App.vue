e4e25c3a1641ad41cb17f02661495ebdcc469f19
kasapeeee <sea.of.clouds55@gmail.com>
2021-11-23 11:28:09 +0900

    add-composition-ref

--- vite_app/src/App.vue
+++ vite_app/src/App.vue
@@ -1,20 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld title="Composition API" msg="This is Composition API sample" />
-    <HelloJSX title="Composition API JSX" msg="This is Composition API JSX"/>
+    <HelloWorld title="Composition API" />
+<!--    <HelloJSX title="Composition API JSX" msg="This is Composition API JSX"/>-->
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import HelloJSX from './components/HelloJSX.jsx'
+// import HelloJSX from './components/HelloJSX.jsx'
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
-    HelloJSX
+    // HelloJSX
   },
 }
 </script>
--- vite_app/src/components/HelloWorld.vue
+++ vite_app/src/components/HelloWorld.vue
@@ -1,19 +1,29 @@
 <template>
   <div class="alert alert-info">
     <h1>{{ title }}</h1>
-    <p>{{ msg }}</p>
+    <p>{{ data.msg }} ({{ data.count }})</p>
   </div>
 </template>
 
 <script>
+import {ref} from 'vue'
+
 export default {
   name: 'HelloWorld',
   props: {
     title: String,
-    msg: String
   },
   setup(props) {
-    console.log(props)
+    const data = ref({
+      msg: 'This is ref-value!',
+      count: 0
+    })
+    setInterval(() => {
+      data.value.count++
+    }, 1000)
+    return {
+      data
+    }
   }
 }
 </script>
